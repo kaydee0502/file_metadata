8b6e7e10dee770e493caa28008942981fd620e1b
kaydee0502 <kshitijdhama513@gmail.com>
2021-08-29 21:03:24 +0530

    file upload

--- server.js
+++ server.js
@@ -1,18 +1,24 @@
 var express = require('express');
 var cors = require('cors');
+const fileUpload = require('express-fileupload');
 require('dotenv').config()
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+app.use(fileUpload());
 
 app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
 
-
+app.post("/api/fileanalyse", (req, res) => {
+  const {name, size, mimetype} = req.files.upfile
+  console.log(req.files.upfile);
+  res.json({name: name, type: mimetype, size: size})
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
